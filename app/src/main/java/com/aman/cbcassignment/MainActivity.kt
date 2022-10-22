c3cd09bd3c200279a71c7d335d1207e76de9076c
aksingla009@gmail.com <aksingla009@gmail.com>
2022-10-22 01:38:57 -0400

    Implemented Recycler View

--- app/src/main/java/com/aman/cbcassignment/MainActivity.kt
+++ app/src/main/java/com/aman/cbcassignment/MainActivity.kt
@@ -1,24 +1,30 @@
 package com.aman.cbcassignment
 
+import android.annotation.SuppressLint
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.aman.cbcassignment.adapter.CBCNewsRecyclerViewAdapter
 import com.aman.cbcassignment.viewmodels.MainViewModel
 import com.aman.cbcassignment.viewmodels.MainViewModelFactory
+import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var mainViewModel: MainViewModel
+    private lateinit var mainViewModel: MainViewModel
+
+    private lateinit var recyclerViewAdapter: CBCNewsRecyclerViewAdapter
 
     @Inject
     lateinit var mainViewModelFactory: MainViewModelFactory
 
+    @SuppressLint("NotifyDataSetChanged")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -27,20 +33,19 @@ class MainActivity : AppCompatActivity() {
 
         mainViewModel = ViewModelProvider(this, mainViewModelFactory).get(MainViewModel::class.java)
 
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-        recyclerView.setBackgroundColor(Color.MAGENTA)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = CBCNewsRecyclerViewAdapter()
+        initRecyclerView()
 
         mainViewModel.newsLiveData.observe(this) {
-            val newsList = it?.listIterator()
-            if (newsList != null) {
-                while (newsList.hasNext()) {
-                    val newsItem = newsList.next()
-                    Log.e("AMAND", newsItem.title)
-                    Log.e("AMANDEEP", newsItem.images.square_140)
-                }
+            if (it != null) {
+                recyclerViewAdapter.setUpdatedNews(it)
+                recyclerViewAdapter.notifyDataSetChanged()
             }
         }
+    }
+
+    private fun initRecyclerView() {
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerViewAdapter = CBCNewsRecyclerViewAdapter()
+        recyclerView.adapter = recyclerViewAdapter
     }
 }
--- app/src/main/java/com/aman/cbcassignment/adapter/CBCNewsRecyclerViewAdapter.kt
+++ app/src/main/java/com/aman/cbcassignment/adapter/CBCNewsRecyclerViewAdapter.kt
@@ -3,27 +3,52 @@ package com.aman.cbcassignment.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.aman.cbcassignment.R
+import com.aman.cbcassignment.model.News
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import kotlinx.android.synthetic.main.news_item_layout.view.*
 
-class CBCNewsRecyclerViewAdapter : RecyclerView.Adapter<CBCNewsItemViewHolder>() {
+class CBCNewsRecyclerViewAdapter :
+    RecyclerView.Adapter<CBCNewsRecyclerViewAdapter.CBCNewsItemViewHolder>() {
+
+    private var newsListData: List<News>? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CBCNewsItemViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val listItem = layoutInflater.inflate(R.layout.news_item_layout,parent,false)
-        return CBCNewsItemViewHolder(listItem)
+        val listItemView =
+            LayoutInflater.from(parent.context).inflate(R.layout.news_item_layout, parent, false)
+        return CBCNewsItemViewHolder(listItemView)
     }
 
     override fun onBindViewHolder(holder: CBCNewsItemViewHolder, position: Int) {
-        holder.titleTV.text = "AMAN"
+        holder.bind(newsListData?.get(position)!!)
     }
 
     override fun getItemCount(): Int {
-        return 5
+        return newsListData?.size ?: 0
+    }
+
+    fun setUpdatedNews(updatedNews: List<News>) {
+        this.newsListData = updatedNews
+    }
+
+    class CBCNewsItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val imageView: ImageView = view.imageView
+        private val dateView: TextView = view.datePublishedTV
+        private val titleView: TextView = view.titleTV
+
+        fun bind(news: News) {
+            dateView.text = news.readablePublishedAt
+            titleView.text = news.title
+
+            Glide.with(imageView)
+                .load(news.images.square_140)
+                .apply(RequestOptions.centerCropTransform())
+                .into(imageView)
+        }
     }
 }
 
-class CBCNewsItemViewHolder(val view: View) : RecyclerView.ViewHolder(view){
-    val titleTV = view.findViewById<TextView>(R.id.titleTV)
-}
